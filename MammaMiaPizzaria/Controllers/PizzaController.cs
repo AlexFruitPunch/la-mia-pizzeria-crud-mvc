ced7e3eb2c5c89d63ccd69a042f927c91a2652e8
alexpagani <alexpaganibr@gmail.com>
2022-05-13 16:38:28 +0200

    Add project files.

---
+++ MammaMiaPizzaria/Controllers/PizzaController.cs
@@ -0,0 +1,62 @@
+﻿using MammaMiaPizzaria.Models;
+using MammaMiaPizzaria.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MammaMiaPizzaria.Controllers
+{
+    public class PizzaController : Controller
+    {
+        [HttpGet]                                                           //dichiariamo che questo metodo vuole fare una GET                                   
+        public IActionResult ListinoPizze()
+        {
+            List<Pizza> Pizze = PizzaData.GetPizze();
+            return View("ListinoPizze", Pizze);
+        }
+
+        [HttpGet]
+        public IActionResult DettaglioPizza(int id)
+        {
+            Pizza pizzaTrovata = null;
+
+            foreach (Pizza Pizza in PizzaData.GetPizze())
+            {
+                if (Pizza.id == id)
+                {
+                    pizzaTrovata = Pizza;
+                    break;
+                }
+            }
+
+            if (pizzaTrovata != null)
+            {
+                return View("DettaglioPizza", pizzaTrovata);
+            }
+            else
+            {
+                return NotFound("il post con id" + id + "non è stato trovato");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("FormPizza");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza nuovaPizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("FormPizza", nuovaPizza);
+            }
+
+            Pizza nuovaPizzaConId = new Pizza(PizzaData.GetPizze().Count, nuovaPizza.Nome, nuovaPizza.Ingredienti, nuovaPizza.immagine, nuovaPizza.Prezzo);
+
+            PizzaData.GetPizze().Add(nuovaPizzaConId);
+
+            return RedirectToAction("ListinoPizze");
+        }
+    }
+}
---
+++ MammaMiaPizzaria/Models/Pizza.cs
@@ -0,0 +1,34 @@
+﻿using MammaMiaPizzaria.Utils.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace MammaMiaPizzaria.Models
+{
+    public class Pizza
+    {
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Il nome della Pizza è obbligatorio")]
+        [StringLength(30, ErrorMessage = "il Nome della Pizza non può avere più di 30 caratteri")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage="Gli ingredienti della pizza sono obbligatori")]
+        [PiuDiUnaParolaAttributoValidazione]
+        public string? Ingredienti { get; set; }
+        public string? immagine { get; set; }
+        public double Prezzo { get; set; }
+
+        public Pizza() 
+        {
+            
+        }
+
+        public Pizza(int id, string Nome, string Ingredienti,string immagine, double Prezzo)
+        {
+            this.id = id;
+            this.Nome = Nome;
+            this.Ingredienti = Ingredienti;
+            this.immagine = immagine;
+            this.Prezzo = Prezzo;
+        }
+    }
+}
