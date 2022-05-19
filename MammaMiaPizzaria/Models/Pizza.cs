516b4bb5d6712e31db908433ef832346f660357f
alexpagani <alexpaganibr@gmail.com>
2022-05-19 12:10:04 +0200

    implementazione del database nel progetto

--- MammaMiaPizzaria/Models/Pizza.cs
+++ MammaMiaPizzaria/Models/Pizza.cs
@@ -1,17 +1,23 @@
 ﻿using MammaMiaPizzaria.Utils.Validation;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MammaMiaPizzaria.Models
 {
+    [Table("pizza")]
+    [Index(nameof(Id), IsUnique = true)]
     public class Pizza
     {
+        [Key]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Il nome della Pizza è obbligatorio")]
-        [StringLength(30, ErrorMessage = "il Nome della Pizza non può avere più di 30 caratteri")]
+        [StringLength(50, ErrorMessage = "il Nome della Pizza non può avere più di 50 caratteri")]
         public string? Nome { get; set; }
 
         [Required(ErrorMessage="Gli ingredienti della pizza sono obbligatori")]
+        [Column(TypeName = "text")]
         [PiuDiUnaParolaAttributoValidazione]
         public string? Ingredienti { get; set; }
 
@@ -20,6 +26,7 @@ namespace MammaMiaPizzaria.Models
         public string? Immagine { get; set; }
 
         [Required(ErrorMessage = "Le pizze devono avere un prezzo")]
+        [Column(TypeName = "decimal(6, 2)")]
         public double Prezzo { get; set; }
 
         public Pizza() 
@@ -27,9 +34,8 @@ namespace MammaMiaPizzaria.Models
             
         }
 
-        public Pizza(int id, string Nome, string Ingredienti,string immagine, double Prezzo)
+        public Pizza(string Nome, string Ingredienti,string immagine, double Prezzo)
         {
-            this.Id = id;
             this.Nome = Nome;
             this.Ingredienti = Ingredienti;
             this.Immagine = immagine;
