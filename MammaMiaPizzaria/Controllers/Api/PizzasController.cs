b8da80f9b6de585bedf3f493a3a1332d16b8537a
alexpagani <alexpaganibr@gmail.com>
2022-05-20 18:03:21 +0200

    inizio impostazione Api

---
+++ MammaMiaPizzaria/Controllers/Api/PizzasController.cs
@@ -0,0 +1,23 @@
+﻿using MammaMiaPizzaria.DataBase;
+using MammaMiaPizzaria.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MammaMiaPizzaria.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizze = new List<Pizza>();
+                return Ok(pizze);
+            }
+                
+        }
+    }
+}
